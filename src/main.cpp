b3f316800e149d9e001d04efa4a3e462643f34d0
Mihail Dascalu <dascaluofficial@gmail.com>
2024-01-13 16:52:16 +0300

    Init

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+﻿#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    glfwInit();
+
+    /* Window config */
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(
+        800, 600, "My Vulkan App", nullptr, nullptr
+    );
+
+    /* Main loop */
+    while (!glfwWindowShouldClose(window))
+        glfwPollEvents();
+
+    /* Clean up */
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
